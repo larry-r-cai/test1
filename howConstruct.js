1a187640566c5a0293744d3332144cf821434c6b
Larry Cai <larry.r.cai@gmail.com>
2022-06-24 10:15:53 -0400

    Fixed howConstruct

--- howConstruct.js
+++ howConstruct.js
@@ -1,9 +1,15 @@
 const howConstruct = (targetString, stringArray)=>{
-    if(targetString.length == 0) return [];
+    console.log(">> Start targetString '"+targetString+"'");
+    if(targetString.length == 0){
+        console.log("<< End targetString '"+targetString+"' Returning []");
+         return [];
+    }
     if(stringArray.includes(targetString)){
         //console.log("Returning true, targetString="+targetString);
+        const result = [targetString];
+        console.log("<< End targetString '"+targetString+"' Returning ["+result+"]");
 
-        return [targetString];
+        return result;
     }
     let containsAnyString = false;
     for(let str of stringArray){
@@ -12,35 +18,54 @@ const howConstruct = (targetString, stringArray)=>{
         };
     }
     if(!containsAnyString){
-        return null;
+        console.log("<< End targetString '"+targetString+"' Returning null");
+       return null;
     } 
     //console.log("Not a base case");
-    let result = false;
-    for(let str of stringArray){
-        const subStr = targetString.split(str);
-        console.log("edge="+str+", childNodes="+subStr);
+    
+    for(let edge of stringArray){
+        const subStr = targetString.split(edge);
+        console.log("edge="+edge+", childNodes="+subStr);
         if(subStr.length == 1){//Can't split
+            console.log("Cannot split "+targetString+" by edge "+edge);
              continue;//Move to next string
         }
         let allChildNodesTrue = true;
         let resultArray = [];
+        let isFirstLoop = true;
         for(let str of subStr){//Return true when all splited nodes are true
             const result = howConstruct(str, stringArray);
             if(result == null){
                 console.log("Child node "+str+" of parent node "+targetString+" can Not be constructed.");
                 allChildNodesTrue = false;
+                //isFirstLoop = true;
                 break;
             }
-            resultArray=resultArray.concat(result);
+            if(isFirstLoop){
+                resultArray= resultArray.concat(result);
+                isFirstLoop = false;
+            }
+            else{
+                resultArray= resultArray.concat(edge).concat(result);
+            }
+            //console.log("Result = "+result);
+            //console.log("ResultArray = "+resultArray);
+
         }
         if(allChildNodesTrue){ 
-            console.log("Node "+targetString+" can be constructed.");
+            //console.log("Node "+targetString+" can be constructed. Returning "+resultArray);
             //memo[targetString] = [subStr[0],str,subStr[1]]; 
-            return resultArray.concat(str);
+            // const finalResult = [];
+            // for(const result of resultArray){
+            //     finalResult.concat(result).concat(edge);
+            // }
+            console.log("<< End targetString '"+targetString+"' Returning ["+resultArray+"]");
+
+            return resultArray;
         }
     }
-
+    console.log("<< End targetString '"+targetString+"' Returning null");
     return null;
 }
 
-console.log(howConstruct("Tesest", ['T', 'es', 's', 't']));
+console.log(howConstruct("Tesest", ['T', 'es', 't']));
