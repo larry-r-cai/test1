41cfb057ac17c5214738efb9aadcf18af206bf07
Larry Cai <larry.r.cai@gmail.com>
2022-06-24 10:27:18 -0400

    Added caching

--- howConstruct.js
+++ howConstruct.js
@@ -1,14 +1,18 @@
-const howConstruct = (targetString, stringArray)=>{
+const howConstruct = (targetString, stringArray, memo={})=>{
     console.log(">> Start targetString '"+targetString+"'");
     if(targetString.length == 0){
         console.log("<< End targetString '"+targetString+"' Returning []");
          return [];
     }
+    if(targetString in memo){
+        console.log("Found in cache for "+targetString);
+        return memo[targetString];
+    }
     if(stringArray.includes(targetString)){
         //console.log("Returning true, targetString="+targetString);
         const result = [targetString];
         console.log("<< End targetString '"+targetString+"' Returning ["+result+"]");
-
+        memo[targetString] = result;
         return result;
     }
     let containsAnyString = false;
@@ -19,6 +23,7 @@ const howConstruct = (targetString, stringArray)=>{
     }
     if(!containsAnyString){
         console.log("<< End targetString '"+targetString+"' Returning null");
+        memo[targetString] = null;
        return null;
     } 
     //console.log("Not a base case");
@@ -34,7 +39,7 @@ const howConstruct = (targetString, stringArray)=>{
         let resultArray = [];
         let isFirstLoop = true;
         for(let str of subStr){//Return true when all splited nodes are true
-            const result = howConstruct(str, stringArray);
+            const result = howConstruct(str, stringArray, memo);
             if(result == null){
                 console.log("Child node "+str+" of parent node "+targetString+" can Not be constructed.");
                 allChildNodesTrue = false;
@@ -60,12 +65,13 @@ const howConstruct = (targetString, stringArray)=>{
             //     finalResult.concat(result).concat(edge);
             // }
             console.log("<< End targetString '"+targetString+"' Returning ["+resultArray+"]");
-
+            memo[targetString] = resultArray;
             return resultArray;
         }
     }
     console.log("<< End targetString '"+targetString+"' Returning null");
+    memo[targetString] = null;
     return null;
 }
 
-console.log(howConstruct("Tesest", ['T', 'es', 't']));
+console.log(howConstruct("Tesestesetett", ['T', 'es', 't','et']));
