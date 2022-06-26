cf24e79974d22fe5be4c587200a169ee7f3d6a06
Larry Cai <larry.r.cai@gmail.com>
2022-06-25 20:36:57 -0400

    Add countConstruct

---
+++ countConstruct.js
@@ -0,0 +1,23 @@
+const countConstruct = (targetString, stringArray, memo={}) => {
+    if(targetString === ''){
+        return 1;
+    }
+    if(targetString in memo){
+        return memo[targetString];
+    }
+
+    let count = 0;
+    for(let str of stringArray){
+        if(targetString.indexOf(str) === 0){
+            const remainder = targetString.slice(str.length);
+            count = count + countConstruct(remainder, stringArray, memo);
+        }
+    }
+
+    memo[targetString] = count;
+    return count;
+
+}
+
+console.log(countConstruct("Tesestesest", ['T', 'es', 's', 't', 'Tesestesest']));
+console.log(countConstruct("aaaa", ['a', 'aa']));
